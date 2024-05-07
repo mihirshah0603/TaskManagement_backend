7a6bde020a250d3aba0de9a74db83f5c35a773b0
mihirshah0603 <shahmihir015@gmail.com>
2024-05-07 14:40:32 +0530

    Cors added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const dotenv = require('dotenv').config();
+const Cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const {errorHandler} = require('./middleware/errorMiddleware')
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(Cors())
 
 app.use('/api/tasks',require('./routes/taskRoutes'));
 app.use('/api/users',require('./routes/userRoutes'))
